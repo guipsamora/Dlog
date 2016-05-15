0855a8bf555cf2954c03f4972eb5da6a183d8118
guipsamora <gui.psamora@gmail.com>
2016-05-14 21:45:31 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongoose = require('mongoose');
+var mongodb = require('mongodb').MongoClient;
+
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+   res.send("Hello World!");
+});
+
+// 
+// app.get('/user', function(req, res){
+//    res.sendStatus("ok");
+// });
+
+
+
+
+
+
+
+// the port that the server is listening to
+app.listen(3000, function(){
+   console.log("App listening on port: 3000");
+});
+
